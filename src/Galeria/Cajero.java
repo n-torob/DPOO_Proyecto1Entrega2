db25715033b10cdcf82702c32ca6fde40eb2282b
n-torob <79644724+n-torob@users.noreply.github.com>
2024-04-14 20:06:09 -0500

    Unificado1

--- src/Galeria/Cajero.java
+++ src/Galeria/Cajero.java
@@ -1,5 +1,33 @@
 package Galeria;
 
+
+
 public class Cajero {
 
+	private String Id;
+	
+	private String metodoPago;
+	
+	public boolean registrarPago(String nombreUsuario, float costo) {
+		
+		usuario balance = new usuario(nombreUsuario);
+		
+		balance.getBalance();
+		
+		if (balance < costo ) {
+			
+		}
+		
+		else {
+			
+		}
+		
+		
+		
+	}
+	
+	
+	
+	
+	
 }
